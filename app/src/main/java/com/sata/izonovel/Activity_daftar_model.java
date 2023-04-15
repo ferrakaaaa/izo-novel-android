247f7a49edd57f0141d1d38f24dcfc7e307e3c36
kesit wijanarko <kesitwjr10@gmail.com>
2023-04-15 16:22:11 +0700

    nambah daftar novel activity_daftar_model.xml

---
+++ app/src/main/java/com/sata/izonovel/Activity_daftar_model.java
@@ -0,0 +1,14 @@
+package com.sata.izonovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_daftar_model extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_model);
+    }
+}
